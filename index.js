4de1e18ee1de928e9d78873887169f72843933f7
bbrouwer <b.brouwer@student.avans.nl>
2017-03-25 18:09:31 +0100

    authentication

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
 mongoose.connect('mongodb://localhost:27017/restrace');
+require('./config/passport')(passport); // pass passport for configuration
+
 
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
@@ -17,14 +19,14 @@ app.use(bodyParser()); // get information from html forms
 //TODO zorg ervoor dan login in een aparte route staat en voeg authenticatie en shit toe 
 //Models
 require('./models/race');
-require('./models/user');
+//require('./models/user');
 require('./models/generateTestData')();
 
 //Routes
 var home = require('./routes/home.js');
 var races = require('./routes/races.js');
 var users = require('./routes/users.js');
-var login = require('./routes/login.js');
+
 
 //Views
 app.set('views', path.join(__dirname, 'views'));
@@ -36,12 +38,12 @@ app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secre
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
+require('./routes/login.js')(app, passport); // Try to pass passport and app AFTER serting up passport/flash
 
 
 
 app.use('/', home);
 app.use('/races', races);
 app.use('/users', users);
-app.use('/login', login);
 
 app.listen(process.env.PORT || 3000);
