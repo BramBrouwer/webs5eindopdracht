5d7c0af7bc1d8e6a2dc0cdce96416d26e9afa69c
Alec <alec@Alec-Laptop>
2017-03-25 17:32:15 +0100

    sdaf

--- index.js
+++ index.js
@@ -5,22 +5,49 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// Data Access Layer
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/restrace');
+mongoose.Promise = require('q').Promise;
 
 //Models
 require('./models/race');
 require('./models/user');
-require('./models/waypoint');
+require('./models/generateTestData')();
 
-// Routes
-var home = require('./routes/home.js');
-var races = require('./routes/races.js');
-var users = require('./routes/users.js');
+//Routes
+app.use('/', require('./routes/home.js'));
+app.use('/races', require('./routes/races.js'));
+app.use('/users', require('./routes/users.js'));
 
-app.use('/', home);
-app.use('/races', races);
-app.use('/users', users);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 app.listen(process.env.PORT || 3000);
--- routes/races.js
+++ routes/races.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var _ = require('underscore');
 var mongoose = require('mongoose');
-var handleError;
 
 //Models
 Race = mongoose.model('Race');
