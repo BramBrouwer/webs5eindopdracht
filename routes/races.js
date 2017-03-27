d440b585fd2487728691afea9ea5b97623b0e44e
bbrouwer <b.brouwer@student.avans.nl>
2017-03-27 21:41:50 +0200

    comprehensive authorization checking

--- routes/races.js
+++ routes/races.js
@@ -11,28 +11,26 @@ Race = mongoose.model('Race');
 
 //Page for creating a new race 
 function getNewRace(req,res){
-var user = new User(req.user);
-	if(user.role == "admin"){
-		res.render('admin/races/new',{bread: ['Races'],user:user});
-	}else{
-		res.render('/');
-	}
+	
+	var user = new User(req.user);
+  	if(user.role != "admin") {res.redirect('/');}
+	res.render('admin/races/new',{bread: ['Races'],user:user});
 }
+
 //Page for adding waypoint to race
 function getNewWaypoint(req,res){
-var user = new User(req.user);
-var id = req.params.id;
-	if(user.role == "admin"){
-		res.render('admin/races/newwaypoint',{bread: ['Races'],user:user,raceId:id});
-	}else{
-		res.render('/');
-	}
+
+	var user = new User(req.user);
+	if(user.role != "admin") {res.redirect('/');}
+	var id = req.params.id;
+	res.render('admin/races/newwaypoint',{bread: ['Races'],user:user,raceId:id});
+	
 }
 
 //Get all races TODO: pagination/filtering
 function getRaces(req, res){
-	var user = new User(req.user);
-    var query = {};
+	
+	var query = {};
 	if(req.params.id){
 		query._id = req.params.id;
 	} 
@@ -53,8 +51,10 @@ function getRaces(req, res){
 		.fail(err => handleError(req, res, 500, err));
 }
 
-//Post new race/add race to database
+//Add new race to database
 function addRace(req, res){
+    if(req.user.role != "admin") {res.redirect('/');}
+	
 	var race = new Race(req.body);
 	race.save()
 		.then(savedRace => {
@@ -67,6 +67,7 @@ function addRace(req, res){
 
 //Get the race object so we can append the new waypoint object to waypoints array
 function getRaceForNewWaypoint(req,res){
+	
 	var race;
 	var query = {};
 	query._id = req.params.id;
@@ -117,5 +118,7 @@ router.route('/:id')
 router.route('/:id/waypoints/new')
 .get(getNewWaypoint)
 .post(getRaceForNewWaypoint);
+
+
 
 module.exports = router;
