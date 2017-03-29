c3f84fad6cb6955a166e64f32d8529b921cf4957
Alec <alec@Alec-Laptop>
2017-03-29 19:58:44 +0200

    Tijdelijke commit

--- routes/home.js
+++ routes/home.js
@@ -8,13 +8,6 @@ function getHome(req, res){
     res.render('home.ejs', { title: 'Home', bread: [], user: user, message: req.flash('loginMessage') });
 }
 
-function getAutocomp(req, res){
-    var user = new User(req.user);
-    res.render('autocomp.ejs', { title: 'Home', bread: [], user: user, message: req.flash('loginMessage') });
-}
-
-
 //Routes
 router.route('/').get(getHome);
-router.route('/autocomp').get(getAutocomp);
 module.exports = router;
