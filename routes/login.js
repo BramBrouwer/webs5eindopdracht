878254bd69298ca7864529891552edb02b14abe7
bbrouwer <b.brouwer@student.avans.nl>
2017-04-01 00:32:13 +0200

    added json response for login callbacks

--- routes/login.js
+++ routes/login.js
@@ -2,20 +2,49 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
-//Functions
+//Get login page (no json response possible)
 function getHome(req, res){
          res.render('login.ejs', { title: 'Login', message: req.flash('loginMessage') });
 }
+//Login success callback
+function getLoginSuccess(req,res){
+    res.status(200);
+    if(isJsonRequest(req)){
+        res.json({response: "Succesfully logged in"});
+    }else{
+          res.redirect('/');
+    }  
+}
+//Login failure callback
+    //werkt flash message nog?
+    // kan je deze in de json meegeven?
+function getloginFailure(req,res){
+    res.status(500);
+    if(isJsonRequest(req)){
+        res.json({response: req.flash('loginMessage')})
+    }else{
+        res.redirect('/');
+    }
+}
+
+
+function isJsonRequest(req){
+      if(req.accepts('html') == 'html'){
+          return false;
+      }
+      return true;
+}
+
+
+
+
 
 //Routes
 router.route('/')
-    .get(getHome);
-
-//Local login
-router.route('/')
+    .get(getHome)
     .post(passport.authenticate('local', {
-        successRedirect : '/', // redirect to the secure profile section
-        failureRedirect : '/', // redirect back to the signup page if there is an error
+        successRedirect : 'login/success', // redirect to the secure profile section
+        failureRedirect : '/login/failure', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
 
@@ -31,5 +60,14 @@ router.route('/google/callback')
                 successRedirect : '/',
                 failureRedirect : '/'
         }));
+        
+router.route('/success')
+      .get(getLoginSuccess);
+
+router.route('/failure')
+    .get(getloginFailure);
+
+
+        
 
 module.exports = router;
