39a2f567cb96be9400e4d4e7b738bb50c4bb3f70
bbrouwer <b.brouwer@student.avans.nl>
2017-04-04 14:11:40 +0200

    twitter auth half

--- config/auth.js
+++ config/auth.js
@@ -7,5 +7,7 @@ module.exports = {
         'clientID'      : '505802495786-fr9e77vv2q7ufa0thvmvvd6u1bqocjgd.apps.googleusercontent.com',
         'clientSecret'  : 'dlCzAaKuvK7PpHHWa5zgN2Bq',
         'callbackURL'   : 'http://localhost:3000/login/google/callback'
+    },
+      'twitterAuth' : {
     }
 };
