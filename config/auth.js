43bfc48520d3e259ddc386f5fa659103ccb43d36
bbrouwer <b.brouwer@student.avans.nl>
2017-04-04 19:40:47 +0200

    datab online/callbacks online

--- config/auth.js
+++ config/auth.js
@@ -6,11 +6,11 @@ module.exports = {
       'googleAuth' : {
         'clientID'      : '505802495786-fr9e77vv2q7ufa0thvmvvd6u1bqocjgd.apps.googleusercontent.com',
         'clientSecret'  : 'dlCzAaKuvK7PpHHWa5zgN2Bq',
-        'callbackURL'   : 'http://localhost:3000/login/google/callback'
+        'callbackURL'   : 'https://webs5eind.herokuapp.com/login/google/callback'
     },
       'twitterAuth' : {
         'consumerKey'       : '3kfGuirnBAafCXbSL9nijflp8',
         'consumerSecret'    : 'NcfGgzBE8SrAS9jeBP5hTn13Hvahn1RsZc9Dx2SmwaMWJL2xok',
-        'callbackURL'       : 'http://localhost:3000/login/twitter/callback'
+        'callbackURL'       : 'https://webs5eind.herokuapp.com/login/twitter/callback'
     }
 };
