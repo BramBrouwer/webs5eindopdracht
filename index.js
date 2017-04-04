f0c57a4d11494cc95f0138fe4ac0ac797266436a
Alec <alec@Alec-Laptop>
2017-04-04 16:51:56 +0200

    auth

--- index.js
+++ index.js
@@ -46,19 +46,19 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 //require('./routes/login.js')(passport); // Try to pass passport and app AFTER serting up passport/flash
 
-// app.use(function (req, res, next) {
-//     // if user is authenticated in the session, carry on
-//     if (req.isAuthenticated())
-//         return next();
-//     if (req.path.startsWith('/login'))
-//         return next();
-//     // if they aren't redirect them to the login page
-//     if(isJsonRequest(req)){
-//         res.json({err: "Please login first"});
-//     }else{
-//         res.redirect('/login');
-//     }
-// });
+app.use(function (req, res, next) {
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+    if (req.path.startsWith('/login'))
+        return next();
+    // if they aren't redirect them to the login page
+    if(isJsonRequest(req)){
+        res.json({err: "Please login first"});
+    }else{
+        res.redirect('/login');
+    }
+});
 
 app.post('/races', requireRole('admin'));
 app.post('/races/*', requireRole('admin'));
@@ -150,4 +150,3 @@ else
 var applicationUrl = 'http://' + domain;
 swagger.configure(applicationUrl, '1.0.0');
 
-//module.exports = app;
